4ac378743dd6e30fe3761b90492783e947877ed6
Joey Spencer <Joseph.Spencer99@gmail.com>
2019-07-22 01:08:13 +0100

    Added basic input and quit function

--- Snake.c
+++ Snake.c
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct snake{
+  int xPos;
+  int yPos;
+}snake;
+
 //Initialises ncurses window
 void windowSetup(){
   initscr();
@@ -22,12 +27,22 @@ void drawScreen(){
   return;
 }
 
+//Gets the players input
+void getInput(char input){
+  switch (input){
+    case 'q':
+      endwin();
+      exit(0);
+  }
+}
+
 //Main method
 int main(){
   windowSetup();
 
   //Main game loop
   while(1){
+    getInput(getch());
     drawScreen();
     refresh();
   }
